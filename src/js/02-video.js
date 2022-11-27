6796ef792959a46ea594aca6fde44bfc45823886
Skynet4241 <skynet4241@gmail.com>
2022-11-27 18:47:37 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,18 @@
 import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(function (error) {
+    console.log(error);
+  });
